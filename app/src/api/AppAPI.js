ee23cc1fbe0ef71b26f21026c476bcc9747620c4
Dennis Johnson <dennis@songawee.com>
2014-10-25 22:00:07 -0600

    Add Q promises

--- app/src/api/AppAPI.js
+++ app/src/api/AppAPI.js
@@ -1,15 +1,16 @@
+var Q = require('q');
+
 module.exports = {
   init: function() {
-    var promise = new Promise(function(resolve, reject) {
+    var promise = new Q.Promise(function(resolve, reject) {
       $.ajax({
+        crossDomain: true,
         dataType: "json",
-        url: 'http://10.0.1.203:3000/api/projects',
-        headers: {
-          'X-Foo': 'bar'
-        },
+        url: 'http://10.0.1.4:3000/api/projects',
         success: function(data) {
           resolve(data)
-        }
+        },
+        type: 'GET'
       });
     });
 
