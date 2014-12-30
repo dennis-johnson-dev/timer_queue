2aefbd0cb4395abfcd63b681cd3b0232a38a34b1
songawee <flapjacks999@gmail.com>
2014-12-30 09:08:34 -0700

    fix(Harmony Flag): Add harmony flag to jsx-loader

--- grunt_webpack.config.js
+++ grunt_webpack.config.js
@@ -11,7 +11,7 @@ module.exports = function(profile) {
     },
     module: {
       loaders: [
-        { test: /\.js$/, loader: "jsx?" }
+        { test: /\.js$/, loader: "jsx?harmony" }
       ]
     },
     output: {
