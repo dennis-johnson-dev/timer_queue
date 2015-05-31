5b80399763a8fc3e0a75be1e2abc88e48290f86d
songawee <dennis@songawee.com>
2015-05-30 20:32:33 -0600

    test(Karma): setup jasmine test framework

---
+++ karma.conf.js
@@ -0,0 +1,38 @@
+module.exports = function(config) {
+  config.set({
+    basePath: './spec/jasmine/',
+    browsers: [
+
+      'Chrome'
+    ],
+    files: [
+      {
+        pattern: 'tests.bundle.js',
+        watched: false,
+      },
+    ],
+    frameworks: [
+      'jasmine',
+    ],
+    preprocessors: {
+      'tests.bundle.js': [
+        'webpack',
+      ],
+    },
+    reporters: [
+      'dots',
+    ],
+    webpack: {
+      module: {
+        loaders: [
+          { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader' },
+        ],
+      },
+      watch: true
+    },
+    webpackServer: {
+      noInfo: true,
+    },
+    singleRun: false
+  });
+};
