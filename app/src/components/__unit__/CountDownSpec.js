6116e3b5f583ab508b7fa83d4ba1577a8c725fb2
songawee <dennis@songawee.com>
2015-07-26 11:44:09 -0600

    test(CountDownSpec): add spec file

--- app/src/components/__unit__/CountDownSpec.js
+++ app/src/components/__unit__/CountDownSpec.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import TestUtils from 'react/lib/ReactTestUtils';
 import CountDown from '../CountDown';
+import Immutable from 'immutable';
 
 describe('hi', () => {
   it('is true', () => {
+    // const countDown = TestUtils.renderIntoDocument(<CountDown />);
     debugger;
+    const project = Immutable.fromJS({
+      id: 123,
+      tasks: [{
+        desc: 'yo',
+        id: 124,
+        time: 3
+      }],
+      title: 'do'
+    });
+    const countDown = React.render(<CountDown project={ project }/>, document.body);
     expect(true).toBe(true);
   });
 })
